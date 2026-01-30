97e2d246df8d30a28e2b870f234be463c1343bee
Eddie Hill <550046@escg.ac.uk>
2026-01-30 06:38:28 +0000

    pause screen code

---
+++ Assets/Scripts/Game/PauseScreen.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PauseScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool active = false;
+    bool changeAnim = false;
+
+    static string InstName = "PauseScreen";
+
+    Vector3 activePos;
+    Vector3 inactivePos;
+
+    void Start()
+    {
+        activePos = new Vector3(0, 0, 0);
+        inactivePos = new Vector3(0, -350, 0);
+        if (active)
+        {
+            transform.localPosition = activePos;
+        }
+        else
+        {
+            transform.localPosition = inactivePos;
+        }
+    }
+    
+    void setActive(bool flag)
+    {
+        active = flag;
+        changeAnim = true;
+        MenuOverlay.SetActive(flag);
+    }
+
+    bool isActive()
+    {
+        return active;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (changeAnim)
+        {
+            if (active)
+            {
+                transform.localPosition = Vector3.Lerp(transform.localPosition, activePos, 0.2f);
+            }
+            else
+            {
+                transform.localPosition = Vector3.Lerp(transform.localPosition, inactivePos, 0.2f);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            setActive(!active);
+        }
+
+        // For now on, the rest of the code needs the inventory to be active
+
+        if (!active)
+        {
+            return;
+        }
+    }
+
+    static PauseScreen getInst()
+    {
+        return GameObject.Find(InstName).GetComponent<PauseScreen>();
+    }
+
+    public static void SetActive(bool flag)
+    {
+        getInst().setActive(flag);
+    }
+
+    public static void Activate()
+    {
+        SetActive(true);
+    }
+
+    public static void Deactivate()
+    {
+        SetActive(false);
+    }
+
+    public static bool IsActive()
+    {
+        return getInst().isActive();
+    }
+}
